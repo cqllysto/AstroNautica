89a7361bdaa451cfa3d1909511d449ecbf18f80e
cqllysto <aidansamuelwright@gmail.com>
2023-12-05 14:20:47 -0600

    kalman filter

--- control/include/control.h
+++ control/include/control.h
@@ -19,4 +19,46 @@ class control {
         control();
 };
 
+// Kalman filter
+#include <Eigen/Dense>
+
+class KalmanFilter {
+public:
+    // Constructor
+    KalmanFilter();
+
+    // Destructor
+    ~KalmanFilter();
+
+    // Initialize the Kalman filter with initial state estimate
+    void initialize(const Eigen::Vector3d& initialState);
+
+    // Perform a Kalman filter update
+    void update(const Eigen::Vector3d& gyro, const Eigen::Vector3d& observation, double dt);
+
+    // Get the current state estimate
+    Eigen::Vector3d getState() const;
+
+private:
+    // State variables
+    Eigen::Vector3d stateEstimate;
+
+    // Process noise covariance matrix (tune based on system characteristics)
+    double Q_roll;
+    double Q_pitch;
+    double Q_yaw;
+
+    // Measurement noise covariance matrix (tune based on sensor characteristics)
+    double R_roll;
+    double R_pitch;
+    double R_yaw;
+
+    // Kalman filter matrices
+    Eigen::Matrix3d H; // Observation matrix
+
+    // Kalman filter functions
+    void stateTransition(const Eigen::Vector3d& gyro, double dt);
+    Eigen::Vector3d observationModel(const Eigen::Vector3d& direction);
+};
+
 #endif
--- control/src/control.cpp
+++ control/src/control.cpp
@@ -10,6 +10,7 @@ PURPOSE: (calculations for the spacecraft attributes)
 #include "../../model/earthSensor/include/sensor.h"
 #include <cmath>
 #include <iostream>
+#include <Eigen/Dense>
 using namespace std;
 
 /* constructor */
@@ -76,4 +77,86 @@ void control::stab3Axis (spacecraft &sc, double targetYaw,
     bangBang(sc, targetPitch, "pitch");
     bangBang(sc, targetYaw, "yaw");
     bangBang(sc, targetRoll, "roll");
+}
+
+
+// Kalman Filtering
+
+using namespace Eigen;
+
+// Kalman filter parameters
+const double alpha = 0.98; // Blending factor
+
+// Process noise covariance (tune based on system characteristics)
+const double Q_roll = 1e-4;
+const double Q_pitch = 1e-4;
+const double Q_yaw = 1e-4;
+
+// Measurement noise covariance (tune based on sensor characteristics)
+const double R_roll = 1e-2;
+const double R_pitch = 1e-2;
+const double R_yaw = 1e-2;
+
+// State transition model: Euler angles update based on gyro rates
+void stateTransition(Vector3d& state, const Vector3d& gyro, double dt) {
+    state += gyro * dt;
+}
+
+// Observation model: Convert direction vector to Euler angles
+Vector3d observationModel(const Vector3d& direction) {
+    Vector3d angles;
+    angles(0) = atan2(direction(1), direction(0)); // Roll
+    angles(1) = atan2(-direction(2), sqrt(direction(0)*direction(0) + direction(1)*direction(1))); // Pitch
+    angles(2) = 0.0; // Yaw (Assuming single-axis sensor)
+    return angles;
+}
+
+// Kalman filter update: Prediction and Correction steps
+void kalmanUpdate(Vector3d& state, const Vector3d& gyro, const Vector3d& observation, double dt) {
+    // Prediction step
+    stateTransition(state, gyro, dt);
+
+    // Calculate predicted observation based on predicted state
+    Vector3d predictedObservation = observationModel(Vector3d::UnitX()); // Example direction vector (X-axis)
+
+    // Correction step (Update state based on sensor measurements)
+    Vector3d innovation = observation - predictedObservation;
+
+    // Kalman gain calculation
+    Matrix3d H; // Observation matrix (Jacobian of observation model)
+    H.setIdentity();
+    
+    Matrix3d K = H * (H.transpose() * H + R_roll * Matrix3d::Identity()).inverse();
+
+    // Update state estimate
+    state += K * innovation;
+
+    // Update covariance matrix (optional if you need to track uncertainty)
+    Matrix3d I = Matrix3d::Identity();
+    I -= K * H;
+}
+
+int main() {
+    // Initial state estimate (roll, pitch, yaw)
+    Vector3d stateEstimate(0.0, 0.0, 0.0);
+
+    // Gyro measurements (angular rates)
+    Vector3d gyro(0.01, 0.02, 0.03); // Example angular rates (rad/s)
+
+    // Earth/Sun sensor measurements (direction vector)
+    Vector3d sunDirection(1.0, 0.0, 0.0); // Example direction vector (X-axis)
+
+    // Convert initial direction vector to Euler angles
+    Vector3d observation = observationModel(sunDirection);
+
+    // Time step (assuming constant)
+    double dt = 0.1;
+
+    // Kalman filter update
+    kalmanUpdate(stateEstimate, gyro, observation, dt);
+
+    // Display the final estimated Euler angles
+    std::cout << "Estimated Euler Angles (Roll, Pitch, Yaw): " << stateEstimate.transpose() << std::endl;
+
+    return 0;
 }
